8feaccc9d3fb197d80704c61145a56b80d94d5c2
Irony Man <ironyman777@gmail.com>
2015-10-07 19:38:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 int Width = 800;
 int Height = 600;
 int Framerate = 60;
+void keyPressed() {
+	keys[keyCode] = true;
+};
+void keyReleased() {
+	keys[keyCode] = false;
+};
+void mousePressed() {
+	
+};
+void mouseReleased() {
+	
+};
 
 void setup() {
 	size(Width, Height);
