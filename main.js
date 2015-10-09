40961f2f125b134b652fd514824076c36ec3d094
Irony Man <ironyman777@gmail.com>
2015-10-08 21:24:14 -0400

    comments!

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+/* Welcome to
+   _____ _______ ______          __  __       _____ _____ _____         _______ ______ 
+  / ____|__   __|  ____|   /\   |  \/  |     |  __ \_   _|  __ \     /\|__   __|  ____|
+ | (___    | |  | |__     /  \  | \  / |     | |__) || | | |__) |   /  \  | |  | |__   
+  \___ \   | |  |  __|   / /\ \ | |\/| |     |  ___/ | | |  _  /   / /\ \ | |  |  __|  
+  ____) |  | |  | |____ / ____ \| |  | |     | |    _| |_| | \ \  / ____ \| |  | |____ 
+ |_____/   |_|  |______/_/    \_\_|  |_|     |_|   |_____|_|  \_\/_/    \_\_|  |______|
+  
+  
+  Sorry for my messy code.  I'm lazy.                                                                                     
+*/
+// Some initial variables...
 int Width = 800;
 int Height = 600;
 int Framerate = 60;
+
+// Functions!  Woot!
 void keyPressed() {
 	keys[keyCode] = true;
 };
@@ -14,11 +28,13 @@ void mouseReleased() {
 	
 };
 
+
+// A bit of setup...
 void setup() {
 	size(Width, Height);
 	frameRate(Framerate);
 };
-
+// Action!
 void draw() {
 
 };
